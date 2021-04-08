e1a46f81f7bc45fe2b5fb182220913f562932601
TajimaRyosuke <east.helman56@gmail.com>
2021-04-08 14:20:56 +0000

    [update]あとはバリデーションチェックのみ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,18 @@
 class BooksController < ApplicationController
+
   def index
-    @book=Book.all
+    @books=Book.all
+    @book=Book.new
   end
 
   def show
     @book=Book.find(params[:id])
   end
 
-  def new
-    @book=Book.new
-  end
-
   def create
-    book=Book.find(params[:id])
-    book.save
-    redirect_to book_path(book.id)
+    @book=Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id), notice: 'Book was successfully created'
   end
 
   def edit
@@ -24,7 +22,7 @@ class BooksController < ApplicationController
   def update
     book=Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    redirect_to book_path(book.id), notice: 'Book was successfully created'
   end
   def destroy
     book=Book.find(params[:id])
@@ -34,6 +32,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    parms.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
